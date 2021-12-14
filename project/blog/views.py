f23dcbaf2c82dc98fdefd5098da813ed2872bfe1
Janmnik <pyjamaman@janmnik.pl>
2021-12-14 14:35:54 +0100

    link to entries added

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
 
 # Create your views here.
